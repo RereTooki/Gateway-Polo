1cc321b2947e47ebbf90423f7041ba00277c91a1
RereTooki <rereloluwa.afolabi.tooki@gmail.com>
2025-03-21 02:17:55 +0100

    Initial commit

---
+++ src/components/Homepage.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "../App.css";
+
+const Homepage = () => {
+  return (
+    <>
+      {" "}
+      <h1 className="text-3xl font-bold text-red-500 underline text-center poppinsFont">
+        {" "}
+        Hello world!
+      </h1>{" "}
+    </>
+  );
+};
+
+export default Homepage;
