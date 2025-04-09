b1e6992033b225baa7cba1aa8d41b75de11aa00b
RereTooki <rereloluwa.afolabi.tooki@gmail.com>
2025-04-09 16:41:53 +0100

    coming soon

--- src/components/MeetTheTeam.tsx
+++ src/components/MeetTheTeam.tsx
@@ -3,6 +3,12 @@ import team1 from "../assets/images/team1.png";
 import team2 from "../assets/images/team2.png";
 import team3 from "../assets/images/team3.png";
 import team4 from "../assets/images/team4.png";
+import partner1 from "../assets/icons/partner1.svg";
+import partner2 from "../assets/icons/partner2.svg";
+import partner3 from "../assets/icons/partner3.svg";
+import partner4 from "../assets/icons/partner4.svg";
+import partner5 from "../assets/icons/partner5.svg";
+import ComingSoon from "./ComingSoon";
 
 type TeamMember = {
   name: string;
@@ -51,11 +57,25 @@ const team: TeamMember[] = [
   // ...add up to 10 members
 ];
 
+type Partner = {
+  name: string;
+  logo: string;
+};
+
+const partners: Partner[] = [
+  { name: "Bet9ja", logo: partner1 },
+  { name: "FCMB", logo: partner2 },
+  { name: "Rite", logo: partner3 },
+  { name: "Bamboo", logo: partner4 },
+  { name: "Nigerian Equestrian Federation", logo: partner5 },
+  // Add more logos as needed
+];
+
 const MeetTheTeam = () => {
   return (
     <>
       <div className="bg-greens sh-screen text-greens px-4 tab:px-8 xxl:px-16 py-12 xxl:py-16 ">
-        <div className="gap-6 xl:gap-10s flex flex-col items-center">
+        <div className="gap-6 xl:gap-10s flex flex-col items-center text-white">
           <div className="sborder-4 font-medium  text-2xl tab:text-4xl lg:text-3xl xl:text-4xl text-center lg:px-0 px-8 pb-8s">
             <h2>Meet The Gateway Team</h2>
           </div>
@@ -66,7 +86,7 @@ const MeetTheTeam = () => {
             </p>
           </div>
         </div>
-        <div className="overflow-x-scroll whitespace-nowrap snap-x snap-mandatory px-4 pb-4 bg-green-950s mt-12 ">
+        <div className="overflow-x-scroll whitespace-nowrap snap-x snap-mandatory px-4 pb-4 bg-green-950s my-12 ">
           <div className="flex gap-4 w-max ">
             {team.map((member, index) => (
               <div
@@ -92,6 +112,33 @@ const MeetTheTeam = () => {
             ))}
           </div>
         </div>
+        <div className="sborder-2 gap-6 xl:gap-10s flex flex-col items-center text-white mb-10">
+          <div className="sborder-4 font-medium  text-2xl tab:text-4xl lg:text-3xl xl:text-4xl text-center lg:px-0 px-8 pb-8s">
+            <h2>Partners</h2>
+          </div>
+        </div>
+        <section className="bg-green-950 px-4 lg:flex flex-col items-center">
+          <div className="overflow-x-auto whitespace-nowrap snap-x snap-mandatory">
+            <div className="flex items-center gap-6 w-max">
+              {partners.map((partner, index) => (
+                <div
+                  key={index}
+                  className="snap-start flex flex-col items-center sborder-4 min-w-[80px]"
+                >
+                  <img
+                    src={partner.logo}
+                    alt={partner.name}
+                    className="w-14 h-14 object-contain"
+                  />
+                  <span className="text-white text-xs mt-1 text-center whitespace-normal">
+                    {partner.name}
+                  </span>
+                </div>
+              ))}
+            </div>
+          </div>
+        </section>
+        <ComingSoon />
       </div>
     </>
   );
